794761a4baf5682bed40069e3088db2c90fcb5cd
ritesh kakade <kakderitesh@gmail.com>
2022-06-07 11:38:04 +0530

    complete

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://riteshkakade:tkWzanpGk19wfLfU@cluster0.lecyb.mongodb.net/Ritesh1994-test", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
